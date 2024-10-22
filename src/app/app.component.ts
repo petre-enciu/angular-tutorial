fefe4ec7ceaaa33c7d4a3e5c3bd1515dc2bd0451
petre-enciu <petre202001@yahoo.com>
2024-10-22 10:42:15 +0000

    stage 10: routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import {HomeComponent} from './home/home.component';
+import {RouterModule} from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [HomeComponent],
+  imports: [HomeComponent, RouterModule],
   template: `
     <main>
       <header class="brand-name">
         <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
       </header>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
