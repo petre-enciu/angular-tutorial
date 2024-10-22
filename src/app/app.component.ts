a6b2491395d5eb3e81f881de8499b4fb82e3649f
petre-enciu <petre202001@yahoo.com>
2024-10-22 13:00:54 +0000

    stage 11: creating home route and details route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import {HomeComponent} from './home/home.component';
-import {RouterModule} from '@angular/router';
+import {RouterModule, RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [HomeComponent, RouterModule],
-  template: `
+  imports: [HomeComponent, RouterLink, RouterOutlet],
+  template: `    
     <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
-      </header>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+        </header>
+      </a>    
       <section class="content">
         <router-outlet></router-outlet>
       </section>
